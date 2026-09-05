b4f9ca7895bc4da741408b60ddb0882813bda738
Harry-LamNgo <harrylam.ngo115@gmail.com>
2026-09-05 22:37:56 +0700

    Update main.go, .gitignore -- Update flow code in main.go -- add .gitignore to project

--- main.go
+++ main.go
@@ -8,23 +8,27 @@ import (
 
 func main() {
 
-	// Combine both bufio.NewScanner and os.Stdin
-	// --> whenever you call scanner.Scan, it will block and wait for User's input
+	// Combine both bufio.NewScanner and os.Stdin--> whenever you call scanner.Scan, it will block and wait for User's input
 
 	scanner := bufio.NewScanner(os.Stdin)
 
 	for {
+		// Need to show "Pokedex > " prompt everytime a command is executed -> wrap that in manual for loop
 		fmt.Print("Pokedex > ")
 
 		var inputText string
-		for scanner.Scan() {
-			inputText = scanner.Text()
-			cleanInput := cleanInput(inputText)
-			fmt.Printf("Your command was: %v \n", cleanInput[0])
+		if scanner.Scan() == false {
+			break
 		}
-		if err := scanner.Err(); err != nil {
-			fmt.Printf("reading standard input: %v", err)
-		}
+		inputText = scanner.Text()
 
+		// Process input
+		cleanInput := cleanInput(inputText)
+
+		fmt.Printf("Your command was: %v \n", cleanInput[0])
+	}
+
+	if err := scanner.Err(); err != nil {
+		fmt.Printf("reading standard input: %v", err)
 	}
 }
